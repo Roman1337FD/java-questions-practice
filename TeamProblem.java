7fe6f90481e4b3ba3cf8e7087e64aac03ec361ce
Roman Kannaujiya <roman.kannaujiya_cs24@gla.ac.in>
2025-09-11 14:25:33 +0530

    team

---
+++ TeamProblem.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TeamProblem {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt(); 
+        int count = 0; 
+        for (int i = 0; i < n; i++) {
+            int petya = sc.nextInt();
+            int vasya = sc.nextInt();
+            int tonya = sc.nextInt();
+
+            int sum = petya + vasya + tonya;
+            if (sum >= 2) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+
+        sc.close();
+    }
+}
